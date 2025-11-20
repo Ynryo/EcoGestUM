f500501af6bf38e96d4de1f0d812abb75e744522
Ynryo <ewenatideklein@gmail.com>
2025-11-20 15:01:09 +0100

    change login backend

--- login/index.php
+++ login/index.php
@@ -1,4 +1,35 @@
-<?php include(dirname(__FILE__, 2) . '/assets/src/files_header.php') ?>
+<?php
+include(dirname(__FILE__, 2) . '/assets/src/files_header.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include dirname(__FILE__, 2) . '/assets/src/conn.php';
+    $username = $_POST['identifiant'];
+    $password = $_POST['password'];
+
+
+    $stmt = $pdo->prepare("SELECT id_utilisateur, mdp_univ, prenom_utilisateur, id_role FROM utilisateur WHERE identifiant = ?");
+    try {
+        $stmt->execute([$username]);
+    } catch (PDOException $e) {
+        die("<p class=\"error\">Erreur SQL : " . $e->getMessage() . "</p>");
+    }
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($user) {
+        if (password_verify($password, $user['mdp_univ'])) {
+            $_SESSION['user_id'] = $user['id_utilisateur'];
+            $_SESSION["user_name"] = $user["prenom_utilisateur"];
+            $_SESSION["id_role"] = $user["id_role"];
+            header("Location: /");
+            exit();
+        } else {
+            $error_message = "Mot de passe invalide.";
+        }
+    } else {
+        $error_message = "No user found with that username.";
+    }
+} ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -15,33 +46,8 @@
     <section class="login-container">
         <div class="login-box">
             <img src="/assets/img/lmu-logo.png" alt="Logo de Le Mans Université">
-            <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                include_once dirname(__FILE__, 2) . '/assets/src/conn.php';
-                $username = $_POST['identifiant'];
-                $password = $_POST['password'];
-
-                $stmt = $pdo->prepare("SELECT id_utilisateur, mdp_univ, prenom_utilisateur, id_role FROM utilisateur WHERE identifiant = :username");
-                $stmt->bindParam(':username', $username);
-                try {
-                    $stmt->execute();
-                } catch (PDOException $e) {
-                    die("<p class=\"error\">Erreur SQL : " . $e->getMessage() . "</p>");
-                }
-                $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-                if ($user) {
-                    if (password_verify($password, $user['mdp_univ'])) {
-                        $_SESSION['user_id'] = $user['id_utilisateur'];
-                        $_SESSION["user_name"] = $user["prenom_utilisateur"];
-                        $_SESSION["id_role"] = $user["id_role"];
-                        header("Location: /");
-                    } else {
-                        echo "<p class=\"error\">Mot de passe invalide.</p>";
-                    }
-                } else {
-                    echo "<p class=\"error\">No user found with that username.</p>";
-                }
+            <?php if (isset($error_message)) {
+                echo "<p class=\"error\">" . $error_message . "</p>";
             }
             ?>
             <form action="" method="POST">
