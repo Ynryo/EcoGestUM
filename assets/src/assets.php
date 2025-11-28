109540911f0a2694319af1d4cfc7b2e7623a485f
Hugo <hugobard35@gmail.com>
2025-11-28 10:59:44 +0100

    change assets for favicon pour la faire reconnaitre du panel

--- assets/src/assets.php
+++ assets/src/assets.php
@@ -4,7 +4,8 @@
 <link rel="stylesheet" href="/assets/css/ariane-link.css">
 <link rel="stylesheet" href="/assets/css/boxs.css">
 <link rel="stylesheet" href="/assets/css/footer.css">
-<link rel="shortcut icon" href="/assets/img/ecogestum-favicon.png" type="image/png">
+<link rel="icon" href="/assets/img/ecogestum-favicon.png" type="image/png" sizes="32x32">
+<link rel="apple-touch-icon" href="/assets/img/ecogestum-favicon.png">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap" />
